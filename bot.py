6ae14efc053f826826d4da655b349795ca5ca8d0
egalecase-web <egalecase@gmail.com>
2026-05-12 23:53:30 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,47 +1,40 @@
-import os
 import asyncio
+import os
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import CommandStart
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 from aiogram.types import WebAppInfo
-from threading import Thread
 
-# Токен
 TOKEN = "8782810618:AAGBA17vhjNc42Cu8Hfu6QOuPPW3Iy1bAcU"
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
-# Заглушка для Render, чтобы не было ошибок порта
-from flask import Flask
-app = Flask(__name__)
-@app.route('/')
-def health_check():
-    return "OK", 200
-
-def run_health_check():
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
-
 @dp.message(CommandStart())
 async def cmd_start(message: types.Message):
     builder = InlineKeyboardBuilder()
+    
+    # Кнопка MiniApp
     builder.row(types.InlineKeyboardButton(
         text="🎮 Играть", 
         web_app=WebAppInfo(url="https://google.com"))
     )
+    
+    # Кнопки Чат и Канал
     builder.row(
         types.InlineKeyboardButton(text="💬 Чат", url="https://t.me/EagleChatCase"),
         types.InlineKeyboardButton(text="📢 Канал", url="https://t.me/EagleCase")
     )
+
     text = (
         "🎁 Выигрывайте NFT-подарки своей мечты!\n\n"
         "Вам доступен ежедневный бесплатный кейс, а также кейсы, апгрейд, и контракты."
     )
+
     await message.answer(text, reply_markup=builder.as_markup())
 
 async def main():
-    # Запускаем "сайт-заглушку" в отдельном потоке
-    Thread(target=run_health_check, daemon=True).start()
+    print("EagleCase запущен...")
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
